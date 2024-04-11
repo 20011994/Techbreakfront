4078b30dd72a570934641d82a9ed5575daf1604b
love20011994 <love.kalsangrah@gmail.com>
2024-04-11 15:31:50 +0530

    pdev commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>redux4444</h1>
-    <h1>react</h1>
-
-
-    </div>
+    <h1>redux</h1>
+      now we start frontend development123
+      <p>jjj</p>
+      <p>kkkk</p>
+      <p>mmmm</p>
+    </div> 
   );
 }
 
