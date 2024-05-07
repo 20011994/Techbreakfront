c8e3f9a778b6e2bbaac6db1d47c711703561d749
love20011994 <love.kalsangrah@gmail.com>
2024-05-07 13:41:48 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 function App() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
-    <h1>redux</h1>
-      now we start frontend development123
-      <p>jjj</p>
-      <p>kkkk</p>
-      <p>mmmm</p>
-      <p>nnnnn</p>
+   <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Item One" value="1" />
+            <Tab label="Item Two" value="2" />
+            <Tab label="Item Three" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+    </Box>
     </div> 
   );
 }
